c0015c65a6f63f0ae5b11b405f548f521e9b4594
AlexKa45 <40323511+AlexKa45@users.noreply.github.com>
2018-07-04 23:53:45 +0300

    Update my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -7,12 +7,26 @@ from flask import Flask, request
 TOKEN = '619217456:AAH3t5-puxFnsjg5AzMFpim0zKo-SCKPsMQ'
 bot = telebot.TeleBot(TOKEN)
 
-def main():
-    @bot.message_handler(commands=['start'])
-    def start(message):
-        bot.send_message(message.chat.id,message.text)
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id,message.text)
+        
+if "HEROKU" in list(os.environ.keys()):
+    logger = telebot.logger
     
-
-if __name__ == "__main__":
-    main()
-    bot.set_webhook(url='https://demo45.herokuapp.com/')
+    server = Flask(__name__)
+    @server.route("/bot", methods=['POST'])
+    def getMessage():
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
+    
+    @server.route("/")
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook(url="https://demo45.herokuapp.com/") # этот url нужно заменить на url вашего Хероку приложения
+        return "?", 200
+    
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+else:
+    bot.remove_webhook()
+    bot.polling(none_stop=True)
