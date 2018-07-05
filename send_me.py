62ef3ba6f8b47a689e89bd88f159d7942f2e3599
AlexKa45 <40323511+AlexKa45@users.noreply.github.com>
2018-07-05 10:18:59 +0300

    Update send_me.py

--- send_me.py
+++ send_me.py
@@ -1,1 +1,17 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import config
 
+fromaddr = "kasperskiialex@gmail.com"
+mypass = "Zoasler2909"
+toaddr = "platonovaleks2909@gmail.com"
+
+def send(message):
+  msg.attach(MIMEText(message, 'plain'))
+  server = smtplib.SMTP('smtp.gmail.com', 587)
+  server.starttls()
+  server.login(fromaddr, mypass)
+  text = msg.as_string()
+  server.sendmail(fromaddr, toaddr, text)
+  server.quit()
