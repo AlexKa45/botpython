e5e08a2cc726cb42fa2dec009f9011d9fdad7d96
AlexKa45 <40323511+AlexKa45@users.noreply.github.com>
2018-07-05 11:38:05 +0300

    Update send_me.py

--- send_me.py
+++ send_me.py
@@ -1,6 +1,14 @@
 import requests
+from flask import Flask, request
+
+app = Flask(__name__)
 
 url='http://i-tek.000webhostapp.com/bot_mail.php?message=Hello&mail=kasperskiialex@gmail.com&head=Test'
-
 s = requests.get(url)
 
+@app.route('/test')
+def get_test_data():
+    print('\nrequest:')
+    print(request)
+    print('request.data:')
+    print(request.data)
